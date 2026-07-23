55fae1dd37b197778870e99c2102a706aee0e2a4
Jake Mackie <jake.wmackie@gmail.com>
2026-07-23 11:44:29 +0100

    fix: ping cmd

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import { DiscordHono } from 'discord-hono'
 
 const app = new DiscordHono()
-  .command('ping', (c) => c.res('Pong! 🏓'))
+
+// Listen for the /ping command sent from Discord
+app.command('ping', (c) => {
+  return c.res('Pong! 🏓')
+})
 
 export default app
--- src/register.ts
+++ src/register.ts
@@ -1,13 +1,11 @@
-import { Command, Option, register } from 'discord-hono'
+import { Command, register } from 'discord-hono'
 
 const commands = [
-  new Command('hello', 'Hello, World!').options(new Option('name', 'Your name')),
-  new Command('help', 'Docs URL'),
+  new Command('ping', 'Responds with Pong!'),
 ]
 
 register(
   commands,
   process.env.DISCORD_APPLICATION_ID,
-  process.env.DISCORD_TOKEN,
-  // process.env.DISCORD_TEST_GUILD_ID,
+  process.env.DISCORD_TOKEN
 )
