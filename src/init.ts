f2eb1db5fc76bf7a9f8ec02f8af90ae61d8e71c1
Jake Mackie <jake.wmackie@gmail.com>
2026-07-23 11:56:06 +0100

    amend: remove more bloat

--- src/init.ts
+++ src/init.ts
@@ -3,7 +3,10 @@ import { createFactory } from 'discord-hono'
 
 export type Env = {
   Bindings: {
-    // Add future bindings here (e.g. KV, D1 databases)
+    DISCORD_APPLICATION_ID: string
+    DISCORD_PUBLIC_KEY: string
+    DISCORD_TOKEN: string
+    DISCORD_TEST_GUILD_ID: string
   }
 }
 
