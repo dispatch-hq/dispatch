8ed317ee44b8ca184a376a6824b9c6c22964352b
jakemackie <jake.wmackie@gmail.com>
2026-07-25 00:16:17 +0100

    feat: update deployment ✨

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import * as handlers from './commands'
 import { factory } from './init'
 
-// Automatically loads all command handlers in src/commands/
 export default factory.discord().loader(Object.values(handlers))
